2c77bd46df387b091d59753bf3fe56cc11fcd776
Moe-Ajam <mahmoud.othman.ajam@gmail.com>
2024-09-15 22:59:21 +0300

    Added database connections

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github/Moe-Ajam/rss-blod-aggregator
 go 1.23.1
 
 require github.com/joho/godotenv v1.5.1
+
+require github.com/lib/pq v1.10.9 // indirect
--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"database/sql"
+	"github/Moe-Ajam/rss-blod-aggregator/internal/database"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	godotenv.Load(".env")
+
+	dbUrl := os.Getenv("CONN")
+
+	db, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		log.Fatal("Could not establish database connection", err)
+	}
+	dbQueries := database.New(db)
+
+	apiCfg := apiConfig{
+		DB: dbQueries,
+	}
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -29,4 +48,3 @@ func main() {
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
 }
-
