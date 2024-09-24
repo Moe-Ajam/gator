f4a6b70024e52ad2d17a45d28116c8bf3ad8b744
Moe-Ajam <mahmoud.othman.ajam@gmail.com>
2024-09-24 20:14:33 +0300

    Reset the application to go to the CLI approach

--- main.go
+++ main.go
@@ -1,51 +1,7 @@
 package main
 
-import (
-	"database/sql"
-	"github/Moe-Ajam/rss-blod-aggregator/internal/database"
-	"log"
-	"net/http"
-	"os"
-
-	"github.com/joho/godotenv"
-	_ "github.com/lib/pq"
-)
-
-type apiConfig struct {
-	DB *database.Queries
-}
+import "fmt"
 
 func main() {
-	godotenv.Load(".env")
-
-	dbUrl := os.Getenv("CONN")
-
-	db, err := sql.Open("postgres", dbUrl)
-	if err != nil {
-		log.Fatal("Could not establish database connection", err)
-	}
-	dbQueries := database.New(db)
-	apiCfg := apiConfig{
-		DB: dbQueries,
-	}
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		log.Fatal("PORT environment variable is not set")
-	}
-
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("POST /v1/users", apiCfg.handlerUsersCreate)
-
-	mux.HandleFunc("GET /v1/healthz", handlerReadiness)
-	mux.HandleFunc("GET /v1/err", handlerErr)
-
-	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: mux,
-	}
-
-	log.Printf("Serving on port: %s\n", port)
-	log.Fatal(srv.ListenAndServe())
+	fmt.Println("Hello World!")
 }
--- sql/schema/002_users_apikey.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
-ALTER TABLE users
-ADD apikey VARCHAR(64)
-NOT NULL UNIQUE
-DEFAULT encode(sha256(random()::text::bytea), 'hex');
-
--- +goose Down
-ALTER TABLE users
-DROP COLUMN apikey;
