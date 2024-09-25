6e8a68bd57e804fb8d7d7058a72576d33628548f
Moe-Ajam <mahmoud.othman.ajam@gmail.com>
2024-09-25 21:54:57 +0300

    Added a configuration and reading/writing to it from the config package

---
+++ internal/config/config.go
@@ -0,0 +1,68 @@
+package config
+
+import (
+	"encoding/json"
+	"os"
+)
+
+const configFileName = ".gatorconfig.json"
+
+type Config struct {
+	DBURL           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
+}
+
+func Read() (Config, error) {
+	filePath, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		return Config{}, err
+	}
+	defer file.Close()
+
+	config := Config{}
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&config)
+	if err != nil {
+		return Config{}, err
+	}
+
+	return config, nil
+}
+
+func (config *Config) SetUser(username string) error {
+	config.CurrentUserName = username
+	return write(*config)
+}
+
+func write(config Config) error {
+	filePath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(config)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func getConfigFilePath() (string, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	return homeDir + "/" + configFileName, nil
+}
